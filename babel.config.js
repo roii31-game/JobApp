626fe201ab6cd89a9ecd40ac1556a01cd1951413
roii31-game <74428415+roii31-game@users.noreply.github.com>
2023-06-24 11:41:08 -0500

    JobSearchApp

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,19 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: ["babel-preset-expo", "module:metro-react-native-babel-preset"],
     plugins: [
       "@babel/plugin-proposal-export-namespace-from",
       "react-native-reanimated/plugin",
       require.resolve("expo-router/babel"),
+      [
+        "module:react-native-dotenv",
+        {
+          envName: "RAPID_API_KEY",
+          moduleName: "@env",
+          path: ".env",
+        },
+      ],
     ],
   };
 };
